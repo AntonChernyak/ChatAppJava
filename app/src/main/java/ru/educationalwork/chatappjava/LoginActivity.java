9c8674480cdd9e0dbecd7dd58a8e37c90d546509
AntonChernyak <anton767@yandex.ru>
2021-02-15 11:45:58 +0300

    test commit

--- app/src/main/java/ru/educationalwork/chatappjava/LoginActivity.java
+++ app/src/main/java/ru/educationalwork/chatappjava/LoginActivity.java
@@ -15,6 +15,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+/**
+ * Коммент для sourceTree
+ */
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
